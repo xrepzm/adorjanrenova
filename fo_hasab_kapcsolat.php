3caed064ea63971ad70dd8d8d1c1543cf7b41da9
Nandor Dudas <x.rep.zm@gmail.com>
2017-07-08 16:09:03 +0200

    file done

--- fo_hasab_kapcsolat.php
+++ fo_hasab_kapcsolat.php
@@ -1,69 +1,62 @@
 <div class="wrapper">
-	<div class="row-fluid">
-		<div class="span12">
-			<div class="tartalom_koz">&nbsp;</div>
-			<strong>Debreceni telephelyünkről indulva, előre szervezetten teljesítjük a megrendeléseket az egész ország területén. Ettől függetlenül sajnos egészen kis javító munkákat nem minden esetben tudunk elvállalni gazdaságosan. Ezért kérjük szíves megértésüket.</strong>
-			<div class="tartalom_koz">&nbsp;</div>
-		</div>
-	</div>
+    <div class="row-fluid">
+        <div class="span12">
+            <div class="tartalom_koz">&nbsp;</div>
+            <h3 style="text-align: center">Tetőszigetelés és palatető felújítás országosan</h3>
+            <strong>Debreceni telephelyünkről indulva, előre szervezetten teljesítjük a megrendeléseket az egész ország területén. Ettől függetlenül sajnos egészen kis javító munkákat nem minden esetben tudunk elvállalni gazdaságosan. Ezért kérjük szíves megértésüket.</strong>
+            <div class="kapcsolat_sarga_box" style="margin: 10px 0">
+                <div class="row-fluid">
+                    <div class="span6">
+                        Ifj. Adorján Zoltán
+                        <br>
+                        +36 70 319 76 90
+                    </div>
+                    <div class="span6">Hívjon bizalommal:<br>
+                        <span class="kicsi">Hétköznap: 8-18-ig</span>
+                        <span class="kicsi">Szombaton: 9-12-ig</span>
+                    </div>
+                    <div class="clear">&nbsp;</div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 <div class="blog_tartalom kapcsolat_csik">
-	<div class="tartalom_span"></div>
-	<div class="clear">&nbsp;</div>
-	<div class="wrapper">
-		<div class="row-fluid">
-			<div class="span6">
-				<img src="images/kapcsolat_kep.jpg" alt="Az egész ország területén teljesítjük a megrendeléseket">
-			</div>
-			<div class="span6">
-				<iframe
-					src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3290131.014154638!2d19.455977752815482!3d47.37651355892868!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47470e4eaf0f8b17%3A0x5618a86c0d42bb2!2sAdorj%C3%A1n+Renova!5e0!3m2!1shu!2shu!4v1487501724096"
-					width="543"
-					height="344"
-					frameborder="0"
-					style="border:0"
-					allowfullscreen
-				></iframe>
-			</div>
-		</div>
-	</div>
-	<div class="tartalom_span"></div>
-	<div class="clear">&nbsp;</div>
+    <div class="tartalom_span"></div>
+    <div class="clear">&nbsp;</div>
+    <div class="wrapper">
+        <div class="row-fluid">
+            <div class="span6">
+                <img src="images/kapcsolat_kep.jpg" alt="Az egész ország területén teljesítjük a megrendeléseket">
+            </div>
+            <div class="span6">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3290131.014154638!2d19.455977752815482!3d47.37651355892868!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47470e4eaf0f8b17%3A0x5618a86c0d42bb2!2sAdorj%C3%A1n+Renova!5e0!3m2!1shu!2shu!4v1487501724096" width="543" height="344" frameborder="0" style="border:0" allowfullscreen></iframe>
+            </div>
+        </div>
+    </div>
+    <div class="tartalom_span"></div>
+    <div class="clear">&nbsp;</div>
 </div>
 <div class="tartalom_span">&nbsp;</div>
 <div class="clear">&nbsp;</div>
 <div class="wrapper">
-	<div class="row-fluid">
-		<div class="span12">
-			<div class="kapcsolat_sarga_box">
-				<div class="row-fluid">
-					<div class="span6">
-						Ifj. Adorján Zoltán
-						<br>
-						+36 70 319 76 90
-					</div>
-					<div class="span6">Hívjon bizalommal:<br>
-						<span class="kicsi">Hétköznap: 8-18-ig</span>
-						<span class="kicsi">Szombaton: 9-12-ig</span>
-					</div>
-					<div class="clear">&nbsp;</div>
-				</div>
-			</div>
-			<div class="tartalom_span">&nbsp;</div>
-		</div>
-	</div>
+    <div class="row-fluid">
+        <div class="span12">
+            <!-- removed .kapcsolat_sarga_box -->
+        </div>
+    </div>
 </div>
 
 <div class="wrapper">
-	<div class="row-fluid">
-		<div class="span12">
-			<strong>
-				<a href="impressum.html">Impressum részletek itt</a>
-			</strong>
-			<div class="tartalom_koz">&nbsp;</div>
-		</div>
-	</div>
+    <div class="row-fluid">
+        <div class="span12">
+            <strong>
+                <a href="impressum.html">Impressum részletek itt</a>
+            </strong>
+            <div class="tartalom_koz">&nbsp;</div>
+        </div>
+    </div>
 </div>
 
 
